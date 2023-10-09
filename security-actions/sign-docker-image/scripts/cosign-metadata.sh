9aa98abef20b4f8a957b74a7996fad86192caaf9
saisatishkarra <saisatish.karra@konghq.com>
2023-10-09 14:29:14 -0500

    feat(slsa/image-sign): Cosign to sign images

---
+++ security-actions/sign-docker-image/scripts/cosign-metadata.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly signature_ext=".sig"
+readonly signing_cert_ext=".crt"
+
+if [[ -n ${ASSET_PREFIX} ]]; then
+
+    signature_file="${ASSET_PREFIX##*/}.${signature_ext}"
+    certificate_file="${ASSET_PREFIX##*/}.${signing_cert_ext}"
+
+    echo "signature_file=${signature_file}" >> $GITHUB_OUTPUT
+    echo "certificate_file=${certificate_file}" >> $GITHUB_OUTPUT
+else 
+    echo '::error ::set input cosign_output_prefix in $0'
+    exit 1
+fi
+
+
+echo "signing_args=${signing_args}" >> $GITHUB_OUTPUT
+
