9e5aff8207f7fca856df832babcc9915123b9689
saisatishkarra <saisatish.karra@konghq.com>
2023-10-10 10:54:14 -0500

    Fix cosign args

--- security-actions/sign-docker-image/scripts/cosign-metadata.sh
+++ security-actions/sign-docker-image/scripts/cosign-metadata.sh
@@ -5,14 +5,23 @@ set -euo pipefail
 readonly signature_ext=".sig"
 readonly signing_cert_ext=".crt"
 
-if [[ -n ${ASSET_PREFIX} ]]; then
+
+signing_args="--yes --tlog-upload=${REKOR_TRANSPARENCY}"
+
+if [[ ${MULTI_PLATFORM} ]]; then
+    signing_args+=" --recursive"
+fi
+
+if [[ -n "${ASSET_PREFIX}" ]] && [[ "${LOCAL_SAVE_COSIGN_ASSETS}" == "true" ]]; then
 
     signature_file="${ASSET_PREFIX##*/}${signature_ext}"
     certificate_file="${ASSET_PREFIX##*/}${signing_cert_ext}"
 
     echo "signature_file=${signature_file}" >> $GITHUB_OUTPUT
     echo "certificate_file=${certificate_file}" >> $GITHUB_OUTPUT
-else 
-    echo '::error ::set input cosign_output_prefix in $0'
-    exit 1
+
+    signing_args+=" --output-certificate=${certificate_file} --output-signature=${signature_file}"
 fi
+
+echo "COSIGN SIGNING ARGS: ${signing_args}"
+echo "cosign_signing_args=${signing_args}" >> $GITHUB_OUTPUT
