b7def0b377d98a22f0184651d6d9c93617312d82
saisatishkarra <saisatish.karra@konghq.com>
2023-10-17 11:46:27 -0500

    feat(SLSA/SEC-973): container image signing action (#65)
    * feat(slsa/image-sign): Keyless image signing using Cosign
    
    filter uniq registry and sign using digest
    
    Always recursively sign manifest digests if present
    
    * update readme for sign action
    
    * fix readme
    
    Signed-off-by: saisatishkarra <saisatish.karra@konghq.com>
    
    * enable signing transaprency by default
    
    Signed-off-by: saisatishkarra <saisatish.karra@konghq.com>
    
    ---------
    
    Signed-off-by: saisatishkarra <saisatish.karra@konghq.com>

---
+++ security-actions/sign-docker-image/scripts/cosign-metadata.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly signature_ext=".sig"
+readonly signing_cert_ext=".crt"
+
+readonly rekor_transparency="true"
+
+# Always Recurisvely sign one/ all manifest digests for docker manifest distribution /list mediaType
+signing_args="--yes --recursive --tlog-upload=${rekor_transparency}"
+
+# if [[ ${MULTI_PLATFORM} ]]; then
+#     signing_args+=" --recursive"
+# fi
+
+if [[ "${LOCAL_SAVE_COSIGN_ASSETS}" == "true" ]]; then
+    if [[ -n "${ASSET_PREFIX}" ]]; then
+        signature_file="${ASSET_PREFIX##*/}${signature_ext}"
+        certificate_file="${ASSET_PREFIX##*/}${signing_cert_ext}"
+    else
+        echo '::error ::set input cosign_output_prefix in $0'
+        exit 1
+    #     signature_file="${ASSET_PREFIX##*/}${signature_ext}"
+    #     certificate_file="${ASSET_PREFIX##*/}${signing_cert_ext}"
+    fi
+
+    echo "signature_file=${signature_file}" >> $GITHUB_OUTPUT
+    echo "certificate_file=${certificate_file}" >> $GITHUB_OUTPUT
+    signing_args+=" --output-certificate=${certificate_file} --output-signature=${signature_file}"
+fi
+
+echo "COSIGN SIGNING ARGS: ${signing_args}"
+echo "cosign_signing_args=${signing_args}" >> $GITHUB_OUTPUT
