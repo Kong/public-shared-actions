f86883fc4db0a86b940f549bc7db857439533299
Pankaj <pankaj.mouriya@konghq.com>
2025-01-28 22:35:08 +0530

    feat(deps): ignore commitlint check for dependabot messages (#220)
    * feat(deps): ignore commitlint check for dependabot messages
    
    * chore(ci): update commitlint.config.js
    
    ---------
    
    Co-authored-by: saisatishkarra <saisatish.karra@konghq.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -13,5 +13,11 @@ module.exports = {
       [...(await getPackages(ctx)), 'release', 'deps', 'ci']
     ]
   },
+// TODO:  Perform ignores based on Commit Author instead of Message to avoid spoofing
+  // Temporary solution: Ignore function to ignore any commit message which match the regex.
+  // Example commit message: `github-actions(deps): bump anchore/scan-action from 4.1.2 to 6.1.0` 
+  ignores: [
+    (message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message),
+  ],
   helpUrl: 'https://github.com/Kong/public-shared-actions',
 }
