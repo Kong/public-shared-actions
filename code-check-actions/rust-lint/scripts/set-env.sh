fc095d45e81a8107a5b710b4b6a67cf4b0cf6aa5
saisatishkarra <saisatish.karra@konghq.com>
2025-02-27 11:10:26 -0600

    feat(ci): gha analyzer action for anti-patterns (#214)
    chore(ci): fix file filter_mode for reviewdog findings
    
    feat(ci): reusable workflow for gha analyzer
    
    feat(ci): add zizmor to precommit hook
    
    fix(ci): synchronize git hooks on pnpm install

--- code-check-actions/rust-lint/scripts/set-env.sh
+++ code-check-actions/rust-lint/scripts/set-env.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 if [[ -n ${manifest_dir} ]]; then
-    echo "manifest_path=${manifest_dir}/Cargo.toml" >> $GITHUB_OUTPUT
+    echo "manifest_path=${manifest_dir}/Cargo.toml" >> "$GITHUB_OUTPUT"
 fi
